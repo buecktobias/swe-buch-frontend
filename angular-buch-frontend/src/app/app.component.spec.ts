8aedc0f09a94dd100ca9c9cff527688010ca7129
Tobias Bück <buecktobias@gmail.com>
2024-10-27 12:27:52 +0100

    testing config

--- angular-buch-frontend/src/app/app.component.spec.ts
+++ angular-buch-frontend/src/app/app.component.spec.ts
@@ -11,21 +11,14 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // @ts-expect-error TS doesnt know the type
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'angular-buch-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // @ts-expect-error TS doesnt know the type
     expect(app.title).toEqual('angular-buch-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, angular-buch-frontend',
-    );
   });
 });
