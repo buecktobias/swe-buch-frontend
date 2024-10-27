27c9ccbae06f658ec459dbfdd69b8dd856fc9a97
Tobias Bück <buecktobias@gmail.com>
2024-10-27 10:59:33 +0100

    changed github workflow! 13

--- backend/postgres/docker-entrypoint-initdb.d/create-db-buch.sql
+++ backend/postgres/docker-entrypoint-initdb.d/create-db-buch.sql
@@ -1,13 +1,23 @@
-DO
-$$
+DO $$
 BEGIN
-    CREATE DATABASE buch;
-EXCEPTION
-    WHEN duplicate_database THEN RAISE NOTICE 'Database buch already exists, skipping.';
-END
+    -- Attempt to create the database
+    PERFORM 1 FROM pg_database WHERE datname = 'buch';
+    IF NOT FOUND THEN
+        CREATE DATABASE buch;
+ELSE
+        RAISE NOTICE 'Database buch already exists, skipping.';
+END IF;
+END $$;
+
+-- Grant privileges on the database to the postgres user
 GRANT ALL ON DATABASE buch TO postgres;
 
+-- Switch to the newly created 'buch' database
+\c buch
+
+-- Create schema and grant privileges
 CREATE SCHEMA IF NOT EXISTS buch AUTHORIZATION postgres;
 GRANT ALL PRIVILEGES ON SCHEMA buch TO postgres;
 
+-- Set the default search path for the postgres role
 ALTER ROLE postgres SET search_path = 'buch';
