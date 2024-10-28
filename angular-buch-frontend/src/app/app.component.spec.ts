a86922c36b359d2f3a8c3d017d50eb839b0ababe
Tobias Bück <buecktobias@gmail.com>
2024-10-28 13:15:09 +0100

    fixed angular tests!

--- angular-buch-frontend/src/app/app.component.spec.ts
+++ angular-buch-frontend/src/app/app.component.spec.ts
@@ -11,14 +11,12 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // @ts-expect-error TS doesnt know the type
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'angular-buch-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // @ts-expect-error TS doesnt know the type
     expect(app.title).toEqual('angular-buch-frontend');
   });
 });
