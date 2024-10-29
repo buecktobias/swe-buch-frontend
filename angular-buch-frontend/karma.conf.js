3399c0ecca95eecefec097a0bae953a70ea12381
Tobias Bück <buecktobias@gmail.com>
2024-10-29 12:09:02 +0100

    SWE-9 improved angular tests!

--- angular-buch-frontend/karma.conf.js
+++ angular-buch-frontend/karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -9,15 +6,12 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
       },
     },
     jasmineHtmlReporter: {
@@ -31,7 +25,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress','spec', 'kjhtml'],
     browsers: ['Chrome'],
     restartOnFileChange: true
   });
