b245d5b9a706bafb10f2b5fe36798a4be323ccfc
Tobias Bück <buecktobias@gmail.com>
2024-10-31 14:02:36 +0100

    changed github caching!

--- angular-buch-frontend/src/main.ts
+++ angular-buch-frontend/src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
 
+loadDevMessages();
+loadErrorMessages();
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
