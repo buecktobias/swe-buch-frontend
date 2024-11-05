a10d0c235cda524d0adfa98d5c8bdb002048e381
Tobias Bück <buecktobias@gmail.com>
2024-11-05 12:08:54 +0100

    added tests for home component!

--- angular-buch-frontend/src/app/pages/home/home.component.spec.ts
+++ angular-buch-frontend/src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { BookService } from '../../services/books/book.service';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +10,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, ApolloTestingModule],
+      providers: [BookService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
